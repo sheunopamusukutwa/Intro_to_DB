73e0024c04c7ff8b160e23be0987f37efdebdbef
SheunopaMusukutwa <scmusukutwa@gmail.com>
2025-08-17 23:40:49 +0200

    Print table definition 2

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,13 @@
--- Prints the full description/DDL of the `books` table
+-- Prints the full description of the table books
+-- from the database `alx_book_store` using INFORMATION_SCHEMA
 
-SHOW CREATE TABLE BOOKS;
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'books';
